c667c7d236192ef1eeb3c71bbd2bf81dbcb2b508
Luka Maret <maret.luka@gmail.com>
2021-11-02 09:02:04 +0100

    feat: add default pages

---
+++ views/default.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!isset($router) || !isset($content)) {
+    exit(0);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="style/style.css" rel="stylesheet">
+    <title>BUT Info Lyon 1</title>
+</head>
+
+<body>
+
+<header>
+
+</header>
+
+<main>
+    <?= $content ?>
+</main>
+
+<footer>
+
+</footer>
+
+</body>
+
+</html>
