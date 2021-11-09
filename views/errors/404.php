d1eb6cce944f8d0c1f29a9a8c3dd16a5325c6c4d
Melvyn27 <melvyn2701@gmail.com>
2021-11-09 08:44:26 +0100

    feat: 404 page
    fix: current variable
    
    
    feat: 404 page

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,1 +1,10 @@
-<h1>Erreur 404</h1>
+<?php
+$current = '404';
+$css = <<<HTML
+<link href="style/404.css" rel="stylesheet" >
+HTML;
+?>
+<div>
+    <img src="img/404.png/">
+    <h1>Page introuvable</h1>
+</div>
