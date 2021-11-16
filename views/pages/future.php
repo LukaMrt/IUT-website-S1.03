c234eba25bf14c35bb7374c6b2a9b9b3f356f62c
Luka Maret <maret.luka@gmail.com>
2021-11-16 09:19:10 +0100

    feat: future page

--- views/pages/future.php
+++ views/pages/future.php
@@ -5,33 +5,75 @@ $css = <<<HTML
 HTML;
 ?>
 
-<h1>Débouchés</h1>
+<h1>Parcours post-BUT</h1>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<img src="img/arrow.png" alt="Image flèche" class="big-img">
+
+<div class="description">
+    <h2>Que faire après l'obtention d'un BUT ?</h2>
+</div>
+
+<div class="elements-title">
+    <div>
+        <img src="img/scholarship.png" alt="Parcours étudiant">
+        <h2>Master</h2>
+    </div>
+
+    <div>
+        <img src="img/engineer.png" alt="Ingénieur">
+        <h2>Écoles d'ingénieur</h2>
+    </div>
+
+    <div>
+        <img src="img/job-search.png" alt="Monde professionnel">
+        <h2>Insertion professionnelle</h2>
+    </div>
+</div>
+
+<div class="elements-description">
+    <p>
+        Il est possible de rentrer en Master après avoir réussi son BUT.
+    </p>
+
+    <p>
+        Les écoles d'ingénieur acceptent des étudiants sortant d'IUT.
+    </p>
+
+    <p>
+        Le BUT est un diplôme professionnalisant reconnu qui permet de travailler dans le monde professionnel après son
+        obtention.
+    </p>
+</div>
+
+<div class="line right"></div>
+
+<div>
+    <p>
+        Après un BUT, une poursuite d’étude possible est un master dans une école publique ou privée ce qui permet de
+        préparer un diplôme de niveau bac +5. L’admission se fait sur dossier.
+    </p>
+    <img src="img/master.png" alt="Image diplôme master">
+</div>
+
+<div class="line left"></div>
+
+<div>
+    <img src="img/engineer2.png" alt="Image diplôme ingénieur">
+    <p>
+        Intégrer une école d’ingénieur est aussi possible après un BUT. Il est à priori possible de profiter de cette
+        passerelle après seulement 2 ans de BUT mais c'est aux écoles de choisir si elles acceptent ou non les
+        étudiants. Le diplôme d’ingénieur se fait dans une école publique ou privée et donne accès à un diplôme de
+        niveau bac +5. L’admission se fait sur dossier.
+    </p>
+</div>
+
+<div class="line right"></div>
+
+<div>
+    <p>
+        Le BUT étant un diplôme professionnalisant reconnu, il est tout à fait possible de entrer dans le monde
+        professionnel avec un diplôme de niveau bac +3 et chercher un métier. Beaucoup d’entreprises recherchent ce type
+        de profil de technicien.
+    </p>
+    <img src="img/coffee-break.png" alt="Image freelance">
+</div>
