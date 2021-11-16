ac2ae1a840dc26756c74043aba410217630fa09c
Luka Maret <maret.luka@gmail.com>
2021-11-16 09:39:31 +0100

    feat: review future

--- views/pages/future.php
+++ views/pages/future.php
@@ -45,35 +45,45 @@ HTML;
     </p>
 </div>
 
-<div class="line right"></div>
+<div class="container">
+    <div class="line right"></div>
 
-<div>
-    <p>
-        Après un BUT, une poursuite d’étude possible est un master dans une école publique ou privée ce qui permet de
-        préparer un diplôme de niveau bac +5. L’admission se fait sur dossier.
-    </p>
-    <img src="img/master.png" alt="Image diplôme master">
+    <div>
+        <p>
+            Après un BUT, une poursuite d’étude possible est un master dans une école publique ou privée ce qui permet
+            de
+            préparer un diplôme de niveau bac +5. L’admission se fait sur dossier.
+        </p>
+        <img src="img/master.png" alt="Image diplôme master">
+    </div>
 </div>
 
-<div class="line left"></div>
+<div class="container">
+    <div class="line left"></div>
 
-<div>
-    <img src="img/engineer2.png" alt="Image diplôme ingénieur">
-    <p>
-        Intégrer une école d’ingénieur est aussi possible après un BUT. Il est à priori possible de profiter de cette
-        passerelle après seulement 2 ans de BUT mais c'est aux écoles de choisir si elles acceptent ou non les
-        étudiants. Le diplôme d’ingénieur se fait dans une école publique ou privée et donne accès à un diplôme de
-        niveau bac +5. L’admission se fait sur dossier.
-    </p>
+    <div>
+        <img src="img/engineer2.png" alt="Image diplôme ingénieur">
+        <p>
+            Intégrer une école d’ingénieur est aussi possible après un BUT. Il est à priori possible de profiter de
+            cette
+            passerelle après seulement 2 ans de BUT mais c'est aux écoles de choisir si elles acceptent ou non les
+            étudiants. Le diplôme d’ingénieur se fait dans une école publique ou privée et donne accès à un diplôme de
+            niveau bac +5. L’admission se fait sur dossier.
+        </p>
+    </div>
 </div>
 
-<div class="line right"></div>
 
-<div>
-    <p>
-        Le BUT étant un diplôme professionnalisant reconnu, il est tout à fait possible de entrer dans le monde
-        professionnel avec un diplôme de niveau bac +3 et chercher un métier. Beaucoup d’entreprises recherchent ce type
-        de profil de technicien.
-    </p>
-    <img src="img/coffee-break.png" alt="Image freelance">
+<div class="container">
+    <div class="line right"></div>
+
+    <div>
+        <p>
+            Le BUT étant un diplôme professionnalisant reconnu, il est tout à fait possible de entrer dans le monde
+            professionnel avec un diplôme de niveau bac +3 et chercher un métier. Beaucoup d’entreprises recherchent ce
+            type
+            de profil de technicien.
+        </p>
+        <img src="img/coffee-break.png" alt="Image freelance">
+    </div>
 </div>
